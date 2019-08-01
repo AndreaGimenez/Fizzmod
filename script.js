33f3019c743f6651567cce4d86341aa1b0fcab88
Andrea Gimenez <gim.andr26@gmail.com>
2019-08-01 09:29:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,27 @@ el callback del click....
 */
 
 var a = document.getElementById("ancora");
+var parr = document.createElement("p");
+var nuevo_div = document.createElement("div")
+var btn_aceptar = document.createElement("button")
+var btn_cancelar = document.createElement("button")
 
 a.addEventListener("click", e => {
-    e.stopPropagation()
-    var nuevo_div = document.createElement("div")
-    var parr = document.createElement("p");
+    e.preventDefault()
     parr.innerHTML = "esta seguro que quiere abandonar la pagina?"
     nuevo_div.appendChild(parr)
-    var btn_aceptar = document.createElement("button")
     btn_aceptar.innerText = "ACEPTAR"
-    var btn_cancelar = document.createAttribute("button")
     btn_cancelar.innerText = "CANCELAR"
     nuevo_div.appendChild(btn_aceptar)
     nuevo_div.appendChild(btn_cancelar)
     document.body.appendChild(nuevo_div)
+    btn_aceptar.addEventListener("click", e => {
+    })
 })
 
-
-
+btn_cancelar.addEventListener("click", e => {
+  document.body.removeChild(nuevo_div)
+})
+btn_aceptar.addEventListener("click", e => {
+  console.dir(a)
+})
